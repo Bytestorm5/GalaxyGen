14f6c41572dddbd2bc4c75ec0f32241d03610dca
Irrom <31070777+Bytestorm5@users.noreply.github.com>
2023-02-05 19:23:40 -0500

    fix dark spots

--- render.py
+++ render.py
@@ -42,11 +42,13 @@ def render():
 
         #check if star was deleted
         #deleted stars still have to have a listing to keep the index of other stars from breaking
+        if len(stars) <= max(h):
+            continue #should never reach here, but won't crash if the json is broken
         if -1 in stars[h[0]] or  -1 in stars[h[1]]:
             continue
 
-        start = pixel_convesion(stars[h[0]])
-        end = pixel_convesion(stars[h[1]])
+        start = pixel_conversion(stars[h[0]])
+        end = pixel_conversion(stars[h[1]])
         
         output_image = cv2.line(output_image, start, end, GRAY, int(STAR_SIZE*0.4), cv2.LINE_AA)
 
@@ -56,7 +58,7 @@ def render():
         output_mask = cv2.line(output_mask, start, end, (B, G, 127), int(STAR_SIZE*0.4))
 
     ## Draw Stars
-    for i in range(len(stars)):
+    for i in range(len(stars)):        
         p = stars[i]
 
         #check if star was deleted
@@ -64,7 +66,7 @@ def render():
         if -1 in p:
             continue
 
-        output_image = cv2.circle(output_image, pixel_convesion(p), STAR_SIZE, (255, 255, 255), -1, cv2.LINE_AA)
+        output_image = cv2.circle(output_image, pixel_conversion(p), STAR_SIZE, (255, 255, 255), -1, cv2.LINE_AA)
 
         B = i // 255
         G = i % 255
@@ -76,27 +78,28 @@ def render():
     output_raw = output_image.copy()
     cv2.imwrite("output_raw.png", output_raw)
 
-    ### Render Countries
-    countries = json.load(open("countries.json"))
-    voronoi = Voronoi([pixel_conversion(star) for star in galaxy['stars']])   
-    
-    density = cv2.resize(cv2.cvtColor(cv2.imread("Distribution.png"), cv2.COLOR_BGR2GRAY), tuple(np.array(SIZE) * int(SCALE)))
-    _, galaxy_mask = cv2.threshold(density, 12, 255, cv2.THRESH_BINARY)
-    galaxy_mask = cv2.cvtColor(galaxy_mask, cv2.COLOR_GRAY2BGR)
-    galaxy_mask = cv2.medianBlur(galaxy_mask, 39)
-    
-    #Country Overlay layer
-    mask = output_raw
+    if "ownership" in galaxy and len(galaxy["ownership"]) > 0:
+        ### Render Countries
+        countries = json.load(open("countries.json"))
+        voronoi = Voronoi([pixel_conversion(star) for star in galaxy['stars']])   
+        
+        density = cv2.resize(cv2.cvtColor(cv2.imread("Distribution.png"), cv2.COLOR_BGR2GRAY), tuple(np.array(SIZE) * int(SCALE)))
+        _, galaxy_mask = cv2.threshold(density, 12, 255, cv2.THRESH_BINARY)
+        galaxy_mask = cv2.cvtColor(galaxy_mask, cv2.COLOR_GRAY2BGR)
+        galaxy_mask = cv2.medianBlur(galaxy_mask, 39)
+        
+        #Country Overlay layer
+        mask = output_raw
 
-    for owner in galaxy["ownership"]:
-        owner_color = countries[owner['id']]['color']
-        for star in owner['systems']:
-            region = np.array(voronoi.vertices[voronoi.regions[voronoi.point_region[star]]])
-            mask = cv2.fillPoly(mask, np.int32([region]), (owner_color[2], owner_color[1], owner_color[0]))
-            mask = cv2.polylines(mask, np.int32([region]), True, (0.45 * owner_color[2], 0.45 * owner_color[1], 0.45 * owner_color[0]), 3, cv2.LINE_AA)
-    mask = cv2.bitwise_and(mask, galaxy_mask)
-    #mask = cv2.medianBlur(mask, 149)  <<<  Stellaris Style; Breaks due to countries being displayed in one mask rather than separately
-    output_image = cv2.addWeighted(output_image, 0.5, mask, 0.5, 0)
+        for owner in galaxy["ownership"]:
+            owner_color = countries[owner['id']]['color']
+            for star in owner['systems']:
+                region = np.array(voronoi.vertices[voronoi.regions[voronoi.point_region[star]]])
+                mask = cv2.fillPoly(mask, np.int32([region]), (owner_color[2], owner_color[1], owner_color[0]))
+                mask = cv2.polylines(mask, np.int32([region]), True, (0.45 * owner_color[2], 0.45 * owner_color[1], 0.45 * owner_color[0]), 3, cv2.LINE_AA)
+        mask = cv2.bitwise_and(mask, galaxy_mask)
+        #mask = cv2.medianBlur(mask, 149)  <<<  Stellaris Style; Breaks due to countries being displayed in one mask rather than separately
+        output_image = cv2.addWeighted(output_image, 0.5, mask, 0.5, 0)
 
     cv2.imwrite("output.png", output_image)
 
